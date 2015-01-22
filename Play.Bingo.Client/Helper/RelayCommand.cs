7de2694c67f88845bfa9c9e4821774101e236c9e
Marcel <aureole82@hotmail.com>
2015-01-22 07:58:02 +0100

    Generate new Bingo card function implemented.

---
+++ Play.Bingo.Client/Helper/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Play.Bingo.Client.Helper
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Func<bool> _canExecute;
+        private readonly Action _execute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action execute) : this(execute, null)
+        {
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke();
+        }
+    }
+}
---
+++ Play.Bingo.Client/ViewModels/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Play.Bingo.Client.Helper;
+
+namespace Play.Bingo.Client.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            GenerateCommand = new RelayCommand(Generate);
+            Generate();
+        }
+
+        #region Bindable properties and commands.
+
+        private BingoCardViewModel _bingoCard;
+
+        public BingoCardViewModel BingoCard
+        {
+            get { return _bingoCard; }
+            set
+            {
+                if (_bingoCard == value) return;
+                _bingoCard = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public RelayCommand GenerateCommand { get; private set; }
+
+        #endregion
+
+        #region Private helper methods.
+
+        private void Generate()
+        {
+            BingoCard = new BingoCardViewModel();
+        }
+
+        #endregion
+    }
+}
